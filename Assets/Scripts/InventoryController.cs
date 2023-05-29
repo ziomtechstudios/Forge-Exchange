0b50f4d213732725249d949a7004851783e57ca8
Chris Kumor <ziomtechstudios@gmail.com>
2023-05-29 18:04:43 -0400

    Removed Update() from Inventory Controller

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -7,13 +7,14 @@ using UnityEngine.InputSystem.Interactions;
 using UnityEngine.InputSystem;
 
 namespace Com.ZiomtechStudios.ForgeExchange{
-    public class InventoryController : MonoBehaviour{
+    public class InventoryController : MonoBehaviour
+    {
         #region Serialized Fields
         [Tooltip("Amount of inventory slots.")][SerializeField] private int inventoryAmnt;
         [Tooltip("Sprite of what background for each slot.")][SerializeField] private Image[] slotImages;
         [Tooltip("Which slot is selected by users.")][SerializeField] private bool[] slotsInUse;
         [Tooltip("Which slots have item(s).")][SerializeField] private bool[] slotsWithItems;
-        [Tooltip("UI Images that show the given sprite of the items in the inventory")] [SerializeField] private Image[] itemImages;
+        [Tooltip("UI Images that show the given sprite of the items in the inventory")][SerializeField] private Image[] itemImages;
         [Tooltip("Prefab correlating to inventoried items.")][SerializeField] private GameObject[] slotPrefabs;
         [Tooltip("ItemController for correlating inventory items")][SerializeField] private ItemController[] itemConts;
         [SerializeField] private PlayerController playerCont;
@@ -21,26 +22,32 @@ namespace Com.ZiomtechStudios.ForgeExchange{
         [Tooltip("Sprite used by slot to indicate there is no item.")][SerializeField] private Sprite noItemSprite;
         #endregion
         #region Private Funcs
-        private bool ToggleHolding(int index){
+        private bool ToggleHolding(int index)
+        {
             //If the slot selected has an item the player holds the item
             playerCont.HoldingItem = (slotsWithItems[index] && !slotsInUse[index]);
             //Update sprite of what player is holding to that of what was in the selected slot
-            playerCont.HoldingPrefab = (playerCont.HoldingItem)?(slotPrefabs[index]):(null);
-            playerCont.HoldingCont = (playerCont.HoldingItem)?(itemConts[index]):(null);
+            playerCont.HoldingPrefab = (playerCont.HoldingItem) ? (slotPrefabs[index]) : (null);
+            playerCont.HoldingCont = (playerCont.HoldingItem) ? (itemConts[index]) : (null);
             return (!slotsInUse[index]);
         }
         //Player selects which slot in their inventory they want to select, makes that obj the one the player is holding
-        private void SelectSlot(int slotIndex){
+        private void SelectSlot(int slotIndex)
+        {
             //Makes it so that no slot is selected
-            if(slotIndex != (-1)){
-                for(int i = 0; i<inventoryAmnt; i++){
-                    slotsInUse[i] = (i == slotIndex)?(ToggleHolding(i)):false;
+            if (slotIndex != (-1))
+            {
+                for (int i = 0; i < inventoryAmnt; i++)
+                {
+                    slotsInUse[i] = (i == slotIndex) ? (ToggleHolding(i)) : false;
                     slotImages[i].fillCenter = !slotsInUse[i];
                 }
             }
             //selected slot is highlighted
-            else{
-                for(int i = 0; i <=(inventoryAmnt-1); i++){
+            else
+            {
+                for (int i = 0; i <= (inventoryAmnt - 1); i++)
+                {
                     slotsInUse[i] = false;
                     slotImages[i].fillCenter = !slotsInUse[i];
                 }
@@ -48,14 +55,18 @@ namespace Com.ZiomtechStudios.ForgeExchange{
         }
         #endregion
         #region Getters/Setters
-        public bool SlotsAreFull{get{return slotsAreFull;}}
+        public bool SlotsAreFull { get { return slotsAreFull; } }
         #endregion
         #region Public funcs
-        public void DroppingItem(){
+        public void DroppingItem()
+        {
             //If the player is holding item we look for coresponding slot holding said item
-            if(playerCont.HoldingItem){
-                for(int i = 0; i<inventoryAmnt; i++){
-                    if((slotPrefabs[i] == playerCont.HoldingPrefab) &&(slotsInUse[i])){
+            if (playerCont.HoldingItem)
+            {
+                for (int i = 0; i < inventoryAmnt; i++)
+                {
+                    if ((slotPrefabs[i] == playerCont.HoldingPrefab) && (slotsInUse[i]))
+                    {
                         //desired slot found
                         //Empty players hands
                         //Empty slot
@@ -70,19 +81,24 @@ namespace Com.ZiomtechStudios.ForgeExchange{
                 }
             }
         }
-        public void SlotItem(){
+        public void SlotItem()
+        {
             //If the player is holding an object and all their slots are not occupied
-            if(playerCont.HoldingItem && !(slotsAreFull)){
+            if (playerCont.HoldingItem && !(slotsAreFull))
+            {
                 //iterating through slots we find the first empty slot
-                for(int i = 0; i<=(inventoryAmnt-1); i++){ 
-                    if(slotsWithItems[i] == false){
+                for (int i = 0; i <= (inventoryAmnt - 1); i++)
+                {
+                    if (slotsWithItems[i] == false)
+                    {
                         //Fill slot with item
                         slotsWithItems[i] = true;
                         slotPrefabs[i] = playerCont.HoldingPrefab;
                         itemConts[i] = playerCont.HoldingCont;
                         itemImages[i].sprite = playerCont.HoldingPrefab.GetComponent<SpriteRenderer>().sprite;
                         //Empty players hands only if the player isnt selecting the slot the item was just slotted into
-                        if(slotsWithItems[i] != slotsInUse[i]){
+                        if (slotsWithItems[i] != slotsInUse[i])
+                        {
                             playerCont.HoldingItem = false;
                             playerCont.HoldingPrefab = null;
                             playerCont.HoldingCont = null;
@@ -90,9 +106,10 @@ namespace Com.ZiomtechStudios.ForgeExchange{
                         break;
                     }
                 }
-            }        
+            }
         }
-        public void OnSelect(InputAction.CallbackContext context){
+        public void OnSelect(InputAction.CallbackContext context)
+        {
             int slot = int.Parse(context.action.name) - 1;
             SelectSlot(slot);
             //Helps avoid non-needed work  
@@ -100,7 +117,8 @@ namespace Com.ZiomtechStudios.ForgeExchange{
         }
         #endregion
         // Start is called before the first frame update
-        void Start(){
+        void Start()
+        {
             playerCont = transform.parent.parent.parent.GetComponent<PlayerController>();
             slotImages = new Image[inventoryAmnt];
             slotsInUse = new bool[inventoryAmnt];
@@ -110,7 +128,8 @@ namespace Com.ZiomtechStudios.ForgeExchange{
             itemImages = new Image[inventoryAmnt];
             slotsAreFull = false;
             //Setting inventory to empty, should change in future when saves are implemented
-            for(int i = 0; i<inventoryAmnt; i++){
+            for (int i = 0; i < inventoryAmnt; i++)
+            {
                 slotImages[i] = transform.Find($"Slot{i}").gameObject.GetComponent<Image>();
                 itemImages[i] = slotImages[i].transform.Find($"Item{i}").gameObject.GetComponent<Image>();
                 slotsInUse[i] = false;
@@ -119,7 +138,5 @@ namespace Com.ZiomtechStudios.ForgeExchange{
                 slotPrefabs[i] = null;
             }
         }
-        void Update(){
-         }
     }
 }
